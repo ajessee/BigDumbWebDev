03f6f648e1de43fbc2f98c91d3eebc6dc29bbfb2
ajessee <andre.isaac.jessee@gmail.com>
2018-03-27 23:24:47 -0400

    update parallax effect to fade background on scroll

--- app/assets/javascripts/fade_in.js
+++ app/assets/javascripts/fade_in.js
@@ -1,42 +1,74 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+
 const scrollDown = (e) => {
   console.log(e.target);
-  document.querySelector('#about-me-container').scrollIntoView({ behavior: 'smooth' })
+  document.querySelector('#about-me-container').scrollIntoView({
+    behavior: 'smooth'
+  });
 }
+
 const fadeInHeaders = () => {
-
-  console.log("DOM fully loaded and parsed");
-
-  const fadeInName = document.querySelector("#fade-in-name");
-
-  const fadeInTitle = document.querySelector("#fade-in-title");
-
   const dropArrow = document.querySelector('.arrow');
-  
-  setTimeout(function(){ 
+  const fadeInName = document.querySelector("#fade-in-name");
+  const fadeInTitle = document.querySelector("#fade-in-title");
+  console.log("DOM fully loaded and parsed");
+  setTimeout(function () {
     fadeInName.style.opacity = 1;
   }, 1000)
-
-  setTimeout(function(){ 
+  setTimeout(function () {
     fadeInTitle.style.opacity = 1;
   }, 2000)
-
-  // setTimeout(function(){ 
-  //   dropArrow.style.opacity = 1;
-  // }, 5000)
-
   dropArrow.addEventListener('click', scrollDown);
-
 };
-
 
 document.addEventListener("DOMContentLoaded", fadeInHeaders);
 
-// $(window).scroll(function(){
-//   $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
-// //250 is fade pixels
-// });
+let tempScrollY = 0;
+
+window.onscroll = function () {
+  const fadeName = document.querySelector("#fade-in-name");
+  const fadeTitle = document.querySelector("#fade-in-title");
+  const fadeBackground = document.querySelector("#background-image-container");
+  let scrollingDown;
+
+  (window.scrollY > tempScrollY) ? scrollingDown = true : scrollingDown = false;
+  tempScrollY = window.scrollY;
+
+  if (window.scrollY > 50 && window.scrollY < 99 && scrollingDown) {
+    fadeName.style.transition = 'opacity 1.5s'
+    fadeTitle.style.transition = 'opacity 1.5s'
+    fadeBackground.style.transition = 'opacity 1s'
+    fadeName.style.opacity = 0;
+    fadeTitle.style.opacity = 0;
+    fadeBackground.style.opacity = 0.8;
+    console.log('down 50')
+  } else if (window.scrollY > 50 && window.scrollY < 99 && !scrollingDown) {
+    fadeName.style.transition = 'opacity 1.5s'
+    fadeTitle.style.transition = 'opacity 1.5s'
+    fadeName.style.opacity = 1;
+    fadeTitle.style.opacity = 1;
+    fadeBackground.style.opacity = 1;
+    console.log('up 50')
+  }
+  else if (window.scrollY > 100  && window.scrollY < 199 && scrollingDown) {
+    fadeBackground.style.opacity = 0.7;
+    console.log('down 100');
+  }
+  else if (window.scrollY > 100  && window.scrollY < 199  && !scrollingDown) {
+    fadeBackground.style.opacity = 0.8;
+    console.log('up 100')
+  }
+  else if (window.scrollY > 200 && window.scrollY < 399 && scrollingDown) {
+    fadeBackground.style.opacity = 0.6;
+    console.log('down 200');
+  }
+  else if (window.scrollY > 200 && window.scrollY < 399 && !scrollingDown) {
+    fadeBackground.style.opacity = 0.7;
+    console.log('up 200')
+  }
 
 
+
+}
