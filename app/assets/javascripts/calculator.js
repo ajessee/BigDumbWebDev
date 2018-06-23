08d3c0fc68aad9fd2efb35c1fdd4037be2296573
ajessee <andre.isaac.jessee@gmail.com>
2018-06-23 15:50:50 -0400

    refactor js to namespace variables and methods, update todo and calculator to be responsive, fix bugs on delete in todo project, add svg star to todo

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,95 +1,95 @@
 const loadLoanCalc = () => {
   if (document.querySelector('#lc-project-container')){
-    const loanForm = document.getElementById('lc-input-form');
-    loanForm.addEventListener('submit', function (e){
-      const loader = document.getElementById('lc-loader');
-      const mainCard = document.getElementById('lc-card');
-      const resultsCard = document.getElementById('lc-results');
-      const errorField = document.getElementById('lc-errors');
-
-      errorField.style.display = 'none';
-      resultsCard.style.display = 'none';
-      mainCard.style.gridTemplateRows = '10% repeat(2, 1fr) 10%';
-      mainCard.style.height = '100%';
-      loader.style.display = 'block';
-
-      setTimeout(calculateResults, 1300);
-
+    setLCVariables();
+    let lc = window.projects.loanCalculator;
+    lc.loanForm.addEventListener('submit', function (e){
+      lc.errorField.style.display = 'none';
+      lc.resultsCard.style.display = 'none';
+      lc.mainCard.style.gridTemplateRows = '10% repeat(2, 1fr) 10%';
+      lc.mainCard.style.height = '100%';
+      lc.loader.style.display = 'block';
+      setTimeout(lc.calculateResults, 1300);
       e.preventDefault();
     });
   };
 }
 
-const calculateResults = (e) => {
+const setLCVariables = () => {
+  lc = {};
+  lc.loader = document.getElementById('lc-loader');
+  lc.mainCard = document.getElementById('lc-card');
+  lc.resultsCard = document.getElementById('lc-results');
+  lc.loanForm = document.getElementById('lc-input-form');
+  lc.errorField = document.getElementById('lc-errors');
+
+  lc.amount = document.getElementById('lc-amount');
+  lc.interest = document.getElementById('lc-interest');
+  lc.years = document.getElementById('lc-years');
+  lc.monthlyPayment = document.getElementById('lc-monthly-payment');
+  lc.totalPayment = document.getElementById('lc-total-payment');
+  lc.totalInterest = document.getElementById('lc-total-interest');
+
+  lc.principal = parseFloat(lc.amount.value);
+  lc.calculatedInterest = parseFloat(lc.interest.value) / 100 / 12;
+  lc.calculatedPayments = parseFloat(lc.years.value) * 12;
+
+  lc.calculateResults = (e) => {
+
+    lc.principal = parseFloat(lc.amount.value);
+    lc.calculatedInterest = parseFloat(lc.interest.value) / 100 / 12;
+    lc.calculatedPayments = parseFloat(lc.years.value) * 12;
   
-  const amount = document.getElementById('lc-amount');
-  const interest = document.getElementById('lc-interest');
-  const years = document.getElementById('lc-years');
-  const monthlyPayment = document.getElementById('lc-monthly-payment');
-  const totalPayment = document.getElementById('lc-total-payment');
-  const totalInterest = document.getElementById('lc-total-interest');
-
-  const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12;
-  const loader = document.getElementById('lc-loader');
-  const resultsCard = document.getElementById('lc-results');
-  const errorField = document.getElementById('lc-errors');
-
-  //Calculate monthly payment
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal * x * calculatedInterest) / (x-1);
-
-  if (isFinite(monthly)) {
-    monthlyPayment.value = addCommas(monthly.toFixed(2));
-    totalPayment.value = addCommas((monthly * calculatedPayments).toFixed(2));
-    totalInterest.value = addCommas(((monthly * calculatedPayments) - principal).toFixed(2));
-    loader.style.display = 'none';
-    resultsCard.style.display = 'grid';
-  }
-  else {
-    loader.style.display = 'none';
-    errorField.style.display = 'block';
-    errorField.innerHTML = 'Oops...looks like there is something wrong with your numbers. Please check them and try again.';
-  }
-
-
-
+    //Calculate monthly payment
+    lc.x = Math.pow(1 + lc.calculatedInterest, lc.calculatedPayments);
+    lc.monthly = (lc.principal * lc.x * lc.calculatedInterest) / (lc.x-1);
   
-}
-
-const addCommas = (value) => {
-  let chars,
-      isDec,
-      dec;
-  let idx = value.indexOf('.');
-  if (idx > -1) {
-    chars = value.substring(0, idx).split("").reverse();
-    dec = value.substring(idx);
-    isDec = true;
-  }
-  else {
-    chars = value.split("").reverse();
-  }
-  let tempArr = [];
-  for (let i = 1; i <=chars.length; i++) {
-    tempArr.push(chars[i-1]);
-    if (i % 3 === 0 && i != chars.length) {
-      tempArr.push(",");
+    if (isFinite(lc.monthly)) {
+      lc.monthlyPayment.value = lc.addCommas(lc.monthly.toFixed(2));
+      lc.totalPayment.value = lc.addCommas((lc.monthly * lc.calculatedPayments).toFixed(2));
+      lc.totalInterest.value = lc.addCommas(((lc.monthly * lc.calculatedPayments) - lc.principal).toFixed(2));
+      lc.loader.style.display = 'none';
+      lc.resultsCard.style.display = 'grid';
+    }
+    else {
+      lc.loader.style.display = 'none';
+      lc.errorField.style.display = 'block';
+      lc.errorField.innerHTML = 'Oops...looks like there is something wrong with your numbers. Please check them and try again.';
+      setTimeout(function(){
+        lc.errorField.style.display = 'none';
+        lc.mainCard.style.gridTemplateRows = '20% auto 10%';
+        lc.mainCard.style.height = '70%';
+      }, 3000)
     }
   }
-    tempArr.push("$");
-  if (isDec) {
-    return tempArr.reverse().join("").concat(dec);
+
+  lc.addCommas = (value) => {
+    let chars,
+        isDec,
+        dec;
+    let idx = value.indexOf('.');
+    if (idx > -1) {
+      chars = value.substring(0, idx).split("").reverse();
+      dec = value.substring(idx);
+      isDec = true;
+    }
+    else {
+      chars = value.split("").reverse();
+    }
+    let tempArr = [];
+    for (let i = 1; i <=chars.length; i++) {
+      tempArr.push(chars[i-1]);
+      if (i % 3 === 0 && i != chars.length) {
+        tempArr.push(",");
+      }
+    }
+      tempArr.push("$");
+    if (isDec) {
+      return tempArr.reverse().join("").concat(dec);
+    }
+    return tempArr.reverse().join("");
   }
-  return tempArr.reverse().join("");
+
+  window.projects.loanCalculator = lc;
 }
 
 document.addEventListener("DOMContentLoaded", loadLoanCalc);
-
-// For some reason DOMContentLoaded is firing early.
-// document.onreadystatechange = function () {
-//   if (document.readyState === "complete") {
-//     loadTodo();
-//   }
-// }
--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,156 +1,152 @@
 const loadTodo = () => {
-  if (document.querySelector('#todo-project-container')){
-    setVariables();
+  if (document.querySelector('#todo-project-container')) {
+    setTodoVariables();
+    let todo = window.projects.todo;
     //load tasks if they exist in local storage
-    setGetDeleteTask(null, 'get');
-    loadEventListeners();
+    todo.setGetDeleteTask(null, 'get');
+    todo.loadEventListeners();
   };
 }
 
-const setVariables = () => {
+const setTodoVariables = () => {
   // Declare UI element variables to be able to do cool stuff to them!
   todo = {}
-  console.log('setVariables');
+  console.log('set todo project variables');
   todo.form = document.querySelector('#todo-task-form');
   todo.taskList = document.querySelector('#tasks-collection');
   todo.clearBtn = document.querySelector('#todo-clear-tasks');
   todo.filter = document.querySelector('#todo-tasks-filter');
   todo.taskInput = document.querySelector('#todo-task-input');
-  window.projects.todo = todo;
-}
 
-// Declare function to add (load) event listeners to elements
-const loadEventListeners = () => {
-  const todo = window.projects.todo;
-  // Listen for the submit event on the form
-  todo.form.addEventListener('submit', addTask);
-  // Listen for click on delete task icon. However, we are going to listen on taskList element and use event delegation to figure out which one to delete. This is because there are many delete task icons that are being added dynamically and we want to target the correct one.
-  todo.taskList.addEventListener('click', deleteTask);
-  // Listen for click on clear all tasks button.
-  todo.clearBtn.addEventListener('click', deleteAllTasks);
-  // Listen for keyup event on filter input to be able to match task with filter query
-  todo.filter.addEventListener('keyup', filterTasks)
-}
-
-const createTask = (newTask) => {
-  const todo = window.projects.todo;
-  // Create new task item and add class for material.css to hook into
-  const task = document.createElement('li');
-  const taskSpan = document.createElement('span');
-  taskSpan.innerHTML = newTask;
-  taskSpan.className = 'task-span-text';
-  task.className = 'collection-item';
-  // Create text node, fill it with content from taskInput field, and append to li
-  task.appendChild(taskSpan);
-
-  // Create a link that will delete the task and have a font awesome 'x' icon
-  const deleteTaskLink = document.createElement('a');
-  deleteTaskLink.className = 'delete-task secondary-content';
-  deleteTaskLink.innerHTML = '<i class="fas fa-times fa-2x"></i>';
-
-  // Append delete link to task
-  task.appendChild(deleteTaskLink);
-
-  // Append task to task list
-  todo.taskList.appendChild(task);
-
-  return task;
-}
-
-const setGetDeleteTask = (taskItem, setGetDelete) => {
-  console.log('set get delete tasks')
-  let tasks;
-  if (localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-  if (setGetDelete === 'set') {
-    tasks.push(taskItem);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  } else if (setGetDelete === 'get') {
-    tasks.forEach(function (task) {
-      createTask(task);
-    });
-  } else if (setGetDelete === 'delete') {
-    tasks.forEach(function (task, index) {
-      if (taskItem.textContent === task) {
-        tasks.splice(index, 1)
-      }
-    });
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+  // Declare function to add (load) event listeners to elements
+  todo.loadEventListeners = () => {
+    // Listen for the submit event on the form
+    todo.form.addEventListener('submit', todo.addTask);
+    // Listen for click on delete task icon. However, we are going to listen on taskList element and use event delegation to figure out which one to delete. This is because there are many delete task icons that are being added dynamically and we want to target the correct one.
+    todo.taskList.addEventListener('click', todo.deleteTask);
+    // Listen for click on clear all tasks button.
+    todo.clearBtn.addEventListener('click', todo.deleteAllTasks);
+    // Listen for keyup event on filter input to be able to match task with filter query
+    todo.filter.addEventListener('keyup', todo.filterTasks)
   }
 
-}
+  todo.createTask = (newTask) => {
+    // Create new task item and add class for material.css to hook into
+    const task = document.createElement('li');
+    const taskSpan = document.createElement('span');
+    taskSpan.innerHTML = newTask;
+    taskSpan.className = 'task-span-text';
+    task.className = 'collection-item';
+    // Create text node, fill it with content from taskInput field, and append to li
+    task.appendChild(taskSpan);
 
-// Declare event handler function to add task to to-do list
-const addTask = (e) => {
+    // Create a link that will delete the task and have a font awesome 'x' icon
+    const deleteTaskLink = document.createElement('a');
+    deleteTaskLink.className = 'delete-task secondary-content';
+    deleteTaskLink.innerHTML = '<i class="fas fa-times fa-2x"></i>';
 
-  const todo = window.projects.todo;
+    // Append delete link to task
+    task.appendChild(deleteTaskLink);
 
-  e.preventDefault();
+    // Append task to task list
+    todo.taskList.appendChild(task);
 
-  // Check if taskInput field is empty, if so, prompt user for input
-  if (todo.taskInput.value === '') {
-    alert('Looks like you haven\'t added a task to your to-do list! Please try again.');
-  } else {
-    let task = createTask(todo.taskInput.value);
-
-    // Store task in local storage
-    setGetDeleteTask(task.textContent, 'set');
-
-    // Clear taskList input 
-    todo.taskInput.value = '';
+    return task;
   }
-}
 
-// Declare event handler to delete task from to-do list
-const deleteTask = (e) => {
-  if (e.target.parentElement.parentElement.classList.contains('delete-task') ||e.target.parentElement.classList.contains('delete-task')) {
-    if (confirm('Delete this task?')) {
-      e.target.parentElement.parentElement.remove();
-      setGetDeleteTask(e.target.parentElement.parentElement, 'delete');
-    }
-  }
-}
-
-const deleteAllTasks = (e) => {
-  const todo = window.projects.todo;
-  if (confirm('You sure you want to delete all tasks?')) {
-    while (todo.taskList.firstChild) {
-      todo.taskList.removeChild(todo.taskList.firstChild);
-    }
-    localStorage.clear();
-  }
-}
-
-const highlightText = (element, query) => {
-  if (!query) {
-    element.innerHTML = element.innerHTML.replace(/<span\b[^>]*>(.*?)<\/span>/g, '$1').trim();
-    return;
-  }
-  const elInnerHTML = element.innerHTML.replace(/<span\b[^>]*>(.*?)<\/span>/g, '$1').trim();
-  const elText = element.textContent.toLowerCase();
-  const elTextNorm = element.textContent;
-  const originalHTML = elInnerHTML.substring(elText.length);
-  const queryRegex = new RegExp(query, "g");
-  const elTextHighlighted = elTextNorm.replace(queryRegex, "<span class=\"highlight\">$&<\/span>");
-  element.innerHTML = elTextHighlighted + originalHTML;
-}
-
-const filterTasks = (e) => {
-  const textLower = e.target.value.toLowerCase();
-  const text = e.target.value;
-
-  document.querySelectorAll('.collection-item').forEach(function (task) {
-    const item = task.textContent;
-    if (item.toLowerCase().indexOf(textLower) != -1) {
-      task.style.display = 'block';
-      highlightText(task, text)
+  todo.setGetDeleteTask = (taskItem, setGetDelete) => {
+    console.log('set get delete tasks')
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
     } else {
-      task.style.display = 'none';
+      tasks = JSON.parse(localStorage.getItem('tasks'));
     }
-  })
+    if (setGetDelete === 'set') {
+      tasks.push(taskItem);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else if (setGetDelete === 'get') {
+      tasks.forEach(function (task) {
+        todo.createTask(task);
+      });
+    } else if (setGetDelete === 'delete') {
+      tasks.forEach(function (task, index) {
+        if (taskItem.textContent === task) {
+          tasks.splice(index, 1)
+        }
+      });
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }
+
+  // Declare event handler function to add task to to-do list
+  todo.addTask = (e) => {
+
+    e.preventDefault();
+
+    // Check if taskInput field is empty, if so, prompt user for input
+    if (todo.taskInput.value === '') {
+      alert('Looks like you haven\'t added a task to your to-do list! Please try again.');
+    } else {
+      let task = todo.createTask(todo.taskInput.value);
+
+      // Store task in local storage
+      todo.setGetDeleteTask(task.textContent, 'set');
+
+      // Clear taskList input 
+      todo.taskInput.value = '';
+    }
+  }
+
+  // Declare event handler to delete task from to-do list
+  todo.deleteTask = (e) => {
+    if (e.target.parentElement.parentElement.classList.contains('delete-task') || e.target.parentElement.classList.contains('delete-task')) {
+      if (confirm('Delete this task?')) {
+        e.target.closest('.collection-item').remove();
+        todo.setGetDeleteTask(e.target.closest('.collection-item'), 'delete');
+      }
+    }
+  }
+
+  todo.deleteAllTasks = (e) => {
+    if (confirm('You sure you want to delete all tasks?')) {
+      while (todo.taskList.firstChild) {
+        todo.taskList.removeChild(todo.taskList.firstChild);
+      }
+      localStorage.clear();
+    }
+  }
+
+  todo.highlightText = (element, query) => {
+    if (!query) {
+      element.innerHTML = element.innerHTML.replace(/<span\b[^>]*>(.*?)<\/span>/g, '$1').trim();
+      return;
+    }
+    const elInnerHTML = element.innerHTML.replace(/<span\b[^>]*>(.*?)<\/span>/g, '$1').trim();
+    const elText = element.textContent.toLowerCase();
+    const elTextNorm = element.textContent;
+    const originalHTML = elInnerHTML.substring(elText.length);
+    const queryRegex = new RegExp(query, "g");
+    const elTextHighlighted = elTextNorm.replace(queryRegex, "<span class=\"highlight\">$&<\/span>");
+    element.innerHTML = elTextHighlighted + originalHTML;
+  }
+
+  todo.filterTasks = (e) => {
+    const textLower = e.target.value.toLowerCase();
+    const text = e.target.value;
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+      const item = task.textContent;
+      if (item.toLowerCase().indexOf(textLower) != -1) {
+        task.style.display = 'block';
+        todo.highlightText(task, text)
+      } else {
+        task.style.display = 'none';
+      }
+    })
+  }
+
+  window.projects.todo = todo;
 }
 
 // This is down here because since I'm using arrow functions, they aren't hoisted and therefore need to be declared before they are called.
