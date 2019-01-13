6ca3d46fad12f205cc1ad88fcc401fb3d845d95f
ajessee <andre.isaac.jessee@gmail.com>
2019-01-13 17:46:20 -0500

    update navbar to shrink icons and add home icon

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -9,14 +9,47 @@ const setUpNav = () => {
 
   const navbar = document.querySelector("#main-nav");
   const navbarOffsetTop = navbar.offsetTop;
-  const navbarOffsetHeight = navbar.offsetHeight;
 
-  const grow = (e) => {
-    e.target.children[1].children[0].setAttribute("transform", "scale(1.3)");
-  }
+  const iconScale = (event, element, scale, translate) => {
+    if (event) {
 
-  const shrink = (e) => {
-    e.target.children[1].children[0].setAttribute("transform", "scale(1)");
+      let eventType = event.type;
+
+      if (event.target.parentElement.classList.contains("navbar-fixed")) {
+
+        let operators = {
+          multiply: function(a,b) {return a * b},
+          divide: function(a,b) {return a / b},
+        }
+
+        let parensRegex = /\((.*)\)/;
+        let iconEl = event.target.querySelector(".icon-grow");
+        let scaleAttrValue = iconEl.getAttribute("transform");
+        let scaleValue = parseFloat(scaleAttrValue.match(parensRegex).pop());
+
+        let newScaleValue;
+        if (eventType === "mouseenter") {
+          newScaleValue = operators.multiply(scaleValue, 1.2).toString();
+        }
+        else {
+          newScaleValue = operators.divide(scaleValue, 1.2).toString();
+        }
+        if (!translate) {
+          iconEl.setAttribute("transform", `scale(${newScaleValue}) translate(50)`);
+        }
+        else {
+          iconEl.setAttribute("transform", `scale(${newScaleValue})`);
+        }
+        
+        
+      }
+      else{
+        event.target.querySelector(".icon-grow").setAttribute("transform", `scale(${scale})`);
+      }
+    }
+    else if (element) {
+      element.setAttribute("transform", `scale(${scale}) translate(${translate})`);
+    }
   }
 
   const scrollTo = (e) => {
@@ -28,20 +61,35 @@ const setUpNav = () => {
     });
   }
 
-
-
   window.onscroll = () => {
 
-    let scrollingDown;
-    let tempScrollY = 0;
+    const allIcons = document.querySelectorAll(".icon-grow");
+    const allIconLabels = document.querySelectorAll(".icon-label");
+    const bdwdIcon = document.querySelector("#bdwdIcon");
 
-    (window.scrollY > tempScrollY) ? scrollingDown = true : scrollingDown = false;
+    const scaleNavbar = (scale, translate) => {
+      allIcons.forEach(function(icon){
+        iconScale(null, icon, scale, translate)
+    })
+    }
+
+    const toggleIconLabels = (display) => {
+      allIconLabels.forEach(function(label){
+        label.style.display = display;
+    })
+    }
 
     if (window.scrollY >= navbarOffsetTop) {
       navbar.classList.add("navbar-fixed");
+      scaleNavbar("0.5", "50");
+      toggleIconLabels("none");
+      bdwdIcon.style.display = "block";
     }
     else if (window.scrollY <= navbarOffsetTop) {
       navbar.classList.remove("navbar-fixed");
+      scaleNavbar("1", "0");
+      toggleIconLabels("block");
+      bdwdIcon.style.display = "none";
     }
   }
   
@@ -49,10 +97,14 @@ const setUpNav = () => {
     let element = document.querySelector('#' + name + '-icon');
     let mobileView = window.matchMedia('(max-width : 767px)').matches;
     if (!mobileView) {
-      element.addEventListener('mouseenter', grow);
-      element.addEventListener('mouseleave', shrink);
+      element.addEventListener('mouseenter', function(event){
+        iconScale(event, null, "1.3")
+      });
+      element.addEventListener('mouseleave', function(event) {
+        iconScale(event, null, "1")
+      });
     }
-    if (name === 'projects' || name === 'resources' || name === 'blog') {
+    if (name === 'projects' || name === 'resources' || name === 'blog' || name === 'home') {
       element.addEventListener('click', scrollTo);
     }
   });
