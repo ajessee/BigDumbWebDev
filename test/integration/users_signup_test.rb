db487f1099111a6a31800d4083d65c95a3f506bc
ajessee <andre.isaac.jessee@gmail.com>
2019-02-11 00:00:45 -0500

    write test for valid user signup submission

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "Valid signup information" do
+    get signup_path, xhr:true
+    assert_difference 'User.count', 1 do # the 1 is a second argument to assert_difference that specifies size of difference
+      post signup_path, xhr: true, params: {user: {
+        name: "First McLucky",
+        email: "first@mclucky.com",
+        password: "password",
+        password_confirmation: "password"
+      }}
+    end
+    assert_equal "text/javascript", @response.content_type
+    assert_template 'users/create'
+    assert_select "a"
+  end
+
   test "Invalid signup information" do
     get signup_path, xhr: true
     assert_no_difference 'User.count' do
@@ -16,4 +32,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/_new'
     assert_template 'shared/_single_error_message'
   end
+
 end
