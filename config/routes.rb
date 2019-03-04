0154a159759e6e282ca545e5a5c1dbde02b7ec89
ajessee <andre.isaac.jessee@gmail.com>
2019-03-03 20:27:33 -0500

    add status code routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -40,13 +40,23 @@ Rails.application.routes.draw do
 
   get 'errors/bad_request'
 
+  get '/400', to: 'errors#bad_request'
+
   get 'errors/unauthorized'
+
+  get '/401', to: 'errors#unauthorized'
 
   get 'errors/forbidden'
 
+  get '/403', to: 'errors#forbidden'
+
   get 'errors/not_found'
 
+  get '/404', to: 'errors#not_found'
+
   get 'errors/internal_server_error'
+
+  get '/500', to: 'errors#internal_server_error'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
