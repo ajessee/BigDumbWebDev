0002e78752c95c31d55e047a7fc57b22b0d0125e
Andre Jessee <ajessee@air.org>
2019-03-17 01:25:24 -0400

    get menu panel to slide in on click

--- app/javascript/application/navigation.js
+++ app/javascript/application/navigation.js
@@ -26,15 +26,30 @@ const setUpNav = () => {
       }
     },
 
+    slideInMenuToggle: function () {
+      if (this.slideInMenu.classList.contains('menu-closed')) {
+        this.slideInMenu.classList.remove('menu-closed');
+        this.slideInMenu.style.boxShadow ='-7px 0px 22px 0px rgba(0,0,0,0.75)';
+      } else {
+        this.slideInMenu.classList.add('menu-closed');
+        this.slideInMenu.style.boxShadow = 'none';
+      }
+    },
+
     init: function (notNew) {
       let self = this;
-      this.aboutMeContainer = document.querySelector("#about-me-container") ? document.querySelector("#about-me-container") : null;
       this.navButtonContainer = document.querySelector('#nav-button-container');
+      this.slideInMenu = document.querySelector('#nav-slide-in-menu');
+      this.aboutMeContainer = document.querySelector('#about-me-container') ? document.querySelector('#about-me-container') : null;
+      
       this.httpErrorContainer = document.querySelector(".http-error-container") ? document.querySelector(".http-error-container") : null;
      
       if (this.aboutMeContainer) {
         document.addEventListener("scroll", function (e) {
           self.onScroll(true);
+        });
+        this.navButtonContainer.addEventListener("click", function (e) {
+          self.slideInMenuToggle();
         });
       
       } else if (this.httpErrorContainer) {
