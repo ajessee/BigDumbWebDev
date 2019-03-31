7bd06a7a030104f0ae6631b30df0d065a177bdd7
ajessee <andre.isaac.jessee@gmail.com>
2019-03-31 00:38:54 -0400

    add attachment upload button to trix editor

---
+++ app/javascript/application/trix.js
@@ -0,0 +1,45 @@
+// In your custom js file, for example:
+// /app/javascript/utils/trix.js
+const trixOnEditorReady = () => {
+  trixAddAttachmentButtonToToolbar();
+};
+
+const trixAddAttachmentButtonToToolbar = () => {
+  let trixBlockButtons = document.querySelector(".trix-button-group--block-tools");
+  const buttonHTML = `
+      <button
+        type="button"
+        class="trix-button trix-button--icon trix-button--icon-attach-files"
+        data-trix-action="x-attach" title="Attach Files"
+        tabindex="-1"
+      >Attach Files</button>
+    `;
+  trixBlockButtons.innerHTML += buttonHTML;
+
+  document.querySelector(".trix-button--icon-attach-files")
+    .addEventListener("click", trixAddAttachment)
+};
+
+const trixAddAttachment = () => {
+  const fileInput = document.createElement("input");
+
+  fileInput.setAttribute("type", "file");
+  fileInput.setAttribute("accept", ".jpg, .png, .gif");
+  fileInput.setAttribute("multiple", "");
+
+  fileInput.addEventListener("change", () => {
+    const {files} = fileInput;
+    Array.from(files).forEach(insertAttachment)
+  });
+
+  fileInput.click()
+};
+
+const insertAttachment = (file) => {
+  const trixEditor = document.querySelector("trix-editor").editor;
+  trixEditor.insertFile(file);
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  trixOnEditorReady();
+});
