a11a81df26ad3070539d0d2aa4265558aaa07a1a
ajessee <andre.isaac.jessee@gmail.com>
2019-11-11 21:56:07 -0500

    figured out how to cast param from checkbox '0'/'1' into boolean in post model using before_validation method to fix issue in production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_000447) do
+ActiveRecord::Schema.define(version: 2019_11_12_020931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
