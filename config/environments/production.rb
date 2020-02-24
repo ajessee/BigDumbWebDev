47092f5bc37898fa438538bb94319693cc0b1739
ajessee <andre.isaac.jessee@gmail.com>
2020-02-23 22:37:19 -0500

    change sendgrid default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Email Config
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'www.bigdumbwebdev.com'
+  host = 'www.bigdumbweb.dev'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
