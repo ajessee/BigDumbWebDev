de93df060940f3db43b6043596643adeff023936
ajessee <andre.isaac.jessee@gmail.com>
2020-10-12 18:14:19 -0400

    add date picker calendar to mobile display of NYC pictures project

---
+++ app/javascript/application/nycPictures.js
@@ -0,0 +1,38 @@
+import flatpickr from "flatpickr";
+const nycPicturesLoader = () => {
+
+  if (event.target.location.pathname === '/scroll3d') return;
+
+  const nycPicturesContainer = document.getElementById('nyc-project-container');
+
+  if (nycPicturesContainer) {
+    console.info('Loading NYC Pictures module');
+    
+    const flatPickerElement = document.getElementById('nyc-project-date-picker');
+    const currentDay = document.getElementById('nyc-pic-current-date').value;
+
+    if (currentDay) {
+      flatpickr(flatPickerElement, {
+        defaultDate: new Date(2016, 0, currentDay),
+        minDate: new Date(2016, 0, 1),
+        maxDate: new Date(2016, 0, 365),
+        dateFormat: "m-d-Y",
+        theme: 'dark'
+      });
+  
+      flatPickerElement.addEventListener("change", (e) => {
+        let oneDay = 1000 * 60 * 60 * 24;
+        let selectedDay = new Date(e.target.value);
+        let startDay = new Date(2016, 0, 0);
+        let diff = selectedDay - startDay;
+        let selectDate = Math.ceil(diff / oneDay);
+        window.location.href = `/projects/a-year-in-nyc?page=${selectDate}`
+      })
+    }
+
+
+
+  }
+
+};
+document.addEventListener("DOMContentLoaded", nycPicturesLoader);
